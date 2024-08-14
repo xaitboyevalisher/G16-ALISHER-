4b10d7155e2d9d21a8bf0dcfdc41798bf86062c7
xaitboyevalisher <xaitboyevalisher4@gmail.com>
2024-08-14 09:18:11 +0500

    O'zgartirishlar tavsifi

--- src/Tabs/MyCards/index.tsx
+++ src/Tabs/MyCards/index.tsx
@@ -1,66 +1,37 @@
 import React from "react";
 import { Box, Text, Grid, Image } from "@chakra-ui/react";
-import card1 from "./images/card1.png";
-import card2 from "./images/card2.png";
-import card3 from "./images/card3.png";
-import card4 from "./images/card4.png";
-import card5 from "./images/card5.png";
-import card6 from "./images/card6.png";
-import card7 from "./images/card7.png";
-import card8 from "./images/card8.png";
+import card1 from "../images/card1.png";
+import card2 from "../images/card2.png";
+import card3 from "../images/card3.png";
+import card4 from "../images/card4.png";
+import card5 from "../images/card5.png";
+import card6 from "../images/card6.png";
+import card7 from "../images/card7.png";
+import card8 from "../images/card8.png";
 
-const cards = [
-  {
-    id: 1,
-    src: card1,
-    holder: "Noman Manzoor",
-    expiry: "02/30",
-  },
-  {
-    id: 2,
-    src: card2,
-    holder: "Noman Manzoor",
-    expiry: "02/30",
-  },
-  {
-    id: 3,
-    src: card3,
-    holder: "Noman Manzoor",
-    expiry: "02/30",
-  },
-  {
-    id: 4,
-    src: card4,
-    holder: "Noman Manzoor",
-    expiry: "02/30",
-  },
-  {
-    id: 5,
-    src: card5,
-    holder: "Noman Manzoor",
-    expiry: "02/30",
-  },
-  {
-    id: 6,
-    src: card6,
-    holder: "Noman Manzoor",
-    expiry: "02/30",
-  },
-  {
-    id: 7,
-    src: card7,
-    holder: "Noman Manzoor",
-    expiry: "02/30",
-  },
-  {
-    id: 8,
-    src: card8,
-    holder: "Noman Manzoor",
-    expiry: "02/30",
-  },
-];
+const cardImages: { [key: number]: string } = {
+  1: card1,
+  2: card2,
+  3: card3,
+  4: card4,
+  5: card5,
+  6: card6,
+  7: card7,
+  8: card8,
+};
 
-const MyCards = () => {
+interface Card {
+  id: number;
+  src: string;
+  holder: string;
+  expiry: string;
+}
+
+interface MyCardsProps {
+  cards: Card[];
+}
+
+const MyCards: React.FC<MyCardsProps> = ({ cards }) => {
   return (
     <>
       <Text fontSize="2xl" fontWeight="bold" mb="4">
@@ -75,7 +46,7 @@ const MyCards = () => {
             boxShadow="md"
             overflow="hidden"
           >
-            <Image src={card.src} alt={`Card ${card.id}`} />
+            <Image src={card.src || "./images/default-card.png"} alt={`Card ${card.id}`} />
             <Box p="4">
               <Text fontWeight="bold">{card.holder}</Text>
               <Text color="gray.500">Expiry Date: {card.expiry}</Text>
